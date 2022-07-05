807b1a10995201da2f9ebdca1512ca1d29ceae32
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2022-07-05 18:48:15 -0400

    Test controlled form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = (props) => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Mugang" animal="Dog" breed="Malamute" />
-      <Pet name="Heena" animal="Dog" breed="Great Pyrenees" />
-      <Pet name="Nayla" animal="Dog" breed="Pit Bull" />
+      <SearchParams />
     </div>
   );
 };
